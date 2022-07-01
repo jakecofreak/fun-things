d4c6d2197898ad2e7c86636b2a0d2adc921147cc
jakecofreak <jakecofreak@yahoo.com>
2022-06-30 21:29:23 -0400

    lecture 7 - dynamic css classes

--- counter-app/src/components/counter.js
+++ counter-app/src/components/counter.js
@@ -7,20 +7,19 @@ class Counter extends Component {
     count: 0,
   };
 
-  styles = {
-    fontSize: 100,
-    fontWeight: "bold",
-  };
-
   render() {
     return (
       <div>
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
